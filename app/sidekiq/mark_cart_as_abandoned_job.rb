2ca7d8cddc40403beb3b47e7596817cdec2022c6
CarolCarmo <carol.acoc@yahoo.com.br>
2025-12-06 15:21:45 -0300

    Firts Pending TEST on cart_item create

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,18 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+  # This job implements the logic to mark carts as abandoned after 3 hours of inactivity
+  # and remove carts that have been abandoned for more than 7 days.
+
+  def perform
+    # Mark carts as abandoned if inactive for more than 3 hours
+    Cart.where('last_interaction_at < ? AND abandoned = ?', 3.hours.ago, false).find_each do |cart|
+      cart.mark_as_abandoned
+    end
+
+    # Remove carts that have been abandoned for more than 7 days
+    Cart.where('last_interaction_at < ? AND abandoned = ?', 7.days.ago, true).find_each do |cart|
+      cart.remove_if_abandoned
+    end
   end
 end
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it 'is valid with quantity greater than 0' do
+      cart = Cart.create!(total_price: 0.0)
+      product = Product.create!(name: 'Valid Qty Product', price: 2.0)
+      ci = CartItem.new(cart: cart, product: product, quantity: 1)
+
+      expect(ci).to be_valid
+    end
+
+    it 'validates uniqueness of product per cart' do
+      cart = Cart.create!(total_price: 0.0)
+      product = Product.create!(name: 'Unique Prod', price: 3.0)
+      CartItem.create!(cart: cart, product: product, quantity: 1)
+
+      duplicate = CartItem.new(cart: cart, product: product, quantity: 2)
+      expect(duplicate).not_to be_valid
+      expect(duplicate.errors[:product_id]).to be_present
+    end
+  end
 end
